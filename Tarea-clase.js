4fdadaeb1206477c905fdfe8eff45858f3787211
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-14 20:40:21 -0500

    Actulizacion

---
+++ Tarea-clase.js
@@ -0,0 +1,22 @@
+//---------------------------
+//Tarea en clase 
+//---------------------------
+
+//Desarrollado por: Yael Mauricio Cano Palacio
+let array = [2,3,4,5,7,8];
+
+function num(array, objectivo) {
+    elements = [];
+    for (let i = 0; i < array.length; i++) {
+        for (let k = 0; k < array.length; k++) {
+            if (array[i] + array[k] == objectivo){
+                elment = [array[i], array[k]];
+                elements.push(elment);
+            }
+            
+        }
+        
+    }
+    return elements;
+}
+console.log(num(array, 10));
