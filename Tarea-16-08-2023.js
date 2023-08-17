7a6dfdc7ea2035b6144fbd5c9a196ee3520a9cac
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-16 20:57:56 -0500

    Actulizacion

---
+++ Tarea-16-08-2023.js
@@ -0,0 +1,28 @@
+
+
+function newPassword(a, b) {
+let result = ''; // La cadena combinada
+
+let i = 0; // Índice para la cadena a
+let j = 0; // Índice para la cadena b
+
+while (i < a.length && j < b.length) {
+    result += a[i] + b[j];
+    i++;
+    j++;
+}
+
+// Agregar los caracteres restantes de la cadena a
+while (i < a.length) {
+    result += a[i];
+    i++;
+}
+
+// Agregar los caracteres restantes de la cadena b
+while (j < b.length) {
+    result += b[j];
+    j++;
+
+}
+
+}
